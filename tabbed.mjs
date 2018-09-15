c9628e3b2ae1a823216463c8fcb1adb5630a6f76
Minifyre <minifyre@gmail.com>
2018-09-15 18:45:57 -0500

    spunoff util

--- tabbed.mjs
+++ tabbed.mjs
@@ -1,18 +1,7 @@
+import util from './tabbed.util.mjs'
 const
 config={},
-output={},
-util=
-{
-	id:()=>([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,util.idHelper),
-	idHelper:c=>(c^util.idRand()[0]&15>>c/4).toString(16),
-	idRand:()=>crypto.getRandomValues(new Uint8Array(1))
-}
-util.findParent=function(el,sel)
-{
-	while (el&&!el.matches(sel)) el=el.parentElement
-	return el
-}
-util.importFiles=paths=>Promise.all(paths.map(x=>fetch(x).then(x=>x.text())))
+output={}
 function input(el,type)
 {
 	const sel=`[data-${type}]`
