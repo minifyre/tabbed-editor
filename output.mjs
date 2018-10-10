7c5d6e5813961694e2733a412f157475e70b75b2
Minifyre <minifyre@gmail.com>
2018-10-10 14:05:19 -0500

    -dead code

--- output.mjs
+++ output.mjs
@@ -42,29 +42,4 @@ output.rerender=function(editor)
 	editor.dom=newDom//@todo pureify
 }
 output.event=(el,evt)=>el.dispatchEvent(new CustomEvent(evt.type,evt))
-output.tab=function(tab)
-{
-	const
-	{el}=util,
-	{id,name}=Object.assign({id:util.id(),name:'untitled'},tab),
-	btn=el('input',{checked:true,id,name:'tabs',type:'radio'}),
-	label=el('label',{className:'tab',innerHTML:name}),
-	icon=el('button',{className:'icon','data-ext':'',innerHTML:'x'})
-	label.setAttribute('data-pointerdown','tabSwitch')
-	icon.setAttribute('data-pointerdown','tabClose')
-	label.setAttribute('for',id)
-	label.prepend(icon)
-	return [btn,label]
-}
-//unlike output.tab, this takes an editor obj
-//@todo kill this off when dom updates become more streamlined
-output.tabs=function({state,shadowRoot:root})
-{
-	const form=root.querySelector('.tabs')
-	state.tabs
-	.map(tab=>output.tab(tab).reverse())//make buttons get added first
-	.reduce((arr,x)=>arr.concat(x),[])//flatten
-	.reverse()//add last items first since they are getting prepended
-	.forEach(el=>form.append(el))
-}
 export default Object.assign(silo,{output})
