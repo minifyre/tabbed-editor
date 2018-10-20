d87efb2d58703238b52fb20d7e043571fca234dd
Minifyre <minifyre@gmail.com>
2018-10-20 13:18:34 -0500

    Update logic.mjs

--- logic.mjs
+++ logic.mjs
@@ -3,8 +3,8 @@ const {config,util}=silo
 function logic(opts={})
 {
 	const state=util.mk(silo.config.state,opts)
-	if(!state.tabs.length) state.tabs.push({id:util.id(),name:'untitled'})
-	if(!state.tab) state.tab=state.tabs[0].id
+	if(!state.file.tabs.length) state.file.tabs.push({id:util.id(),name:'untitled'})
+	if(!state.view.tab) state.view.tab=state.file.tabs[0].id
 	return state
 }
 //@todo use util.mk
@@ -12,8 +12,8 @@ logic.tabCreate=opts=>Object.assign({id:util.id(),name:'untitled'},opts)
 logic.tabClose=function(state,id)
 {
 	const//always have one tab
-	{tabs}=state,
-	switchTabs=state.tab===id,
+	{tabs}=state.file,
+	switchTabs=state.view.tab===id,
 	mkTab=switchTabs&&tabs.length===1
 	//@todo find a way to oneline with tabSwitch, without breaking indexes
 	if(mkTab) logic.tabNew(state)
@@ -22,20 +22,20 @@ logic.tabClose=function(state,id)
 	if(!mkTab) logic.tabSwitch(state,tabs[i+1<tabs.length?i+1:i-1].id)
 	//close tab
 	tabs.splice(i,1)
-	return state.tab
+	return state.view.tab
 }
 logic.tabNew=function(state,opts)
 {
 	const newTab=logic.tabCreate(opts)
-	state.tabs.unshift(newTab)
+	state.file.tabs.unshift(newTab)
 	logic.tabSwitch(state,newTab.id)
 	return newTab.id
 }
 logic.tabSwitch=function(state,id)
 {
-	const newTab=state.tab!==id
-	if(newTab) state.tab=id
+	const newTab=state.view.tab!==id
+	if(newTab) state.view.tab=id
 	return newTab
 }
-logic.toggleFullscreen=state=>state.fullscreen=!state.fullscreen
+logic.toggleFullscreen=state=>state.view.fullscreen=!state.view.fullscreen
 export default Object.assign(silo,{logic})
