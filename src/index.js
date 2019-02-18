020270dc3487f534b8a4dbeb5377ee5195192e0c
Minifyre <minifyre@gmail.com>
2019-02-18 17:39:02 -0600

    +easy curry

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import silo from './node_modules/silo/index.js'
 import truth from './node_modules/truth/truth.mjs'
 import v from './node_modules/v/v.mjs'
 
-const {config,util,logic,output,input}=silo
+const
+{config,util,logic,output,input}=silo,
+{curry}=util
 
 export default silo(async function tabbed(initialState)
 {
